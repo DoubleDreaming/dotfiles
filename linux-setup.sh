75fd360cdf9703a29dd1c7ee0922df2523078e1d
undo <www.undo@daydream.com>
2025-11-09 23:41:24 +0800

    add linux config

---
+++ linux-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install fish
+# Set as default shell
+# chsh -s $(which fish)
+
+# fastfetch, Ubuntu
+# sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+# sudo apt update
+# sudo apt install fastfetch
